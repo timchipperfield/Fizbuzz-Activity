54e466f89a49a72105679ad72fcaa45175e6a2a5
srMarquinho <sr.marquinho@gmail.com>
2016-06-21 12:37:38 +0100

    second-day

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,31 +1,46 @@
-describe('FizzBuzz', function(){
-
+describe('FizzBuzz', function() {
   var fizzBuzz;
 
   beforeEach(function() {
     fizzBuzz = new FizzBuzz();
   });
-  describe('mutiples of 3', function() {
-    it('returns fizz', function() {
-      expect(fizzBuzz.play(3)).toEqual('fizz');
+
+  describe('when multiples 3 and 5', function() {
+    it('displays FizBuzz', function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    });
+
+  it('displays FizBuzz', function() {
+      expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
     });
   });
 
-  describe('multiples of 5', function() {
-    it('returns buzz', function() {
-      expect(fizzBuzz.play(5)).toEqual('buzz');
+  describe('when multiple of 5', function() {
+    it('displays Buzz', function() {
+      expect(fizzBuzz.play(5)).toEqual('Buzz');
+    });
+
+    it('displays Buzz', function() {
+      expect(fizzBuzz.play(25)).toEqual('Buzz');
     });
   });
 
-  describe('multiples of 3 and 5', function() {
-    it('returns fizzbuzz', function() {
-      expect(fizzBuzz.play(15)).toEqual('fizzbuzz');
+  describe('when multiple of 3', function() {
+    it('displays Fizz', function() {
+      expect(fizzBuzz.play(3)).toEqual('Fizz');
+    });
+
+    it('displays Fizz', function() {
+      expect(fizzBuzz.play(9)).toEqual('Fizz');
     });
   });
 
-  describe('all other numbers', function() {
-    it('returns the number', function() {
+  describe('when any other number', function() {
+    it('displays the number itself', function() {
       expect(fizzBuzz.play(1)).toEqual(1);
+    });
+    it('displays the number itself', function() {
+      expect(fizzBuzz.play(7)).toEqual(7);
     });
   });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,15 +1,17 @@
-function FizzBuzz() {
-}
+var FizzBuzz = function() {
+};
 
-FizzBuzz.prototype.play = function(number) {
-  if (number % 15 === 0) {
-    return 'fizzbuzz';
+FizzBuzz.prototype = {
+  play: function(number) {
+    if(number % 15 === 0) {
+      return this.play = 'FizzBuzz'
+    };
+    if(number % 5 === 0) {
+      return this.play = 'Buzz';
+    };
+    if(number % 3 === 0) {
+      return this.play = 'Fizz';
+    };
+    return this.play = number;
   }
-  if ( number % 3 === 0) {
-    return 'fizz';
-  }
-  if ( number % 5 === 0) {
-    return 'buzz';
-  }
-  return number
-}
+};
